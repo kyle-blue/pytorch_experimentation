a13559b89149511aaa80c84b205c7ab43ee924c4
Kyle Doidge <kyle.blue.nuttall@gmail.com>
2023-04-18 23:58:05 +0100

    Dev dependencies and add CIFAR10 dataset

--- pytorch_experimentation/__main__.py
+++ pytorch_experimentation/__main__.py
@@ -1,2 +1,24 @@
+import torch
+from torch import nn
+from torchvision import datasets, transforms
+import os
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
 if __name__ == "__main__":
-    print("You have ligma")
+    train_dataset = datasets.CIFAR10(
+        root="data", download=True, transform=transforms.ToTensor(), train=True
+    )
+    test_dataset = datasets.CIFAR10(
+        root="data", download=True, transform=transforms.ToTensor(), train=False
+    )
+    print(train_dataset.data.shape)
+    print(train_dataset.targets.shape)
+    print(test_dataset.data.shape)
